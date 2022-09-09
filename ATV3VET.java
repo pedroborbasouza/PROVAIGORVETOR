4456bad9b170b7fe8d209d750200703969f7ce6a
hello gatas <101436038+pedroborbasouza@users.noreply.github.com>
2022-09-08 21:38:21 -0300

    Add files via upload

---
+++ ATV3VET.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ATV3VET {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe a massa original do material radioativo");
+        float massaInicial = sc.nextFloat();
+        float massaFinal;
+        int[] tempo = new int[3];
+        int segundos=0, minutos=0;
+
+
+        float massa = massaInicial;
+
+        while (massa > 0.5){
+                massa /= 2;
+                tempo[3] += 50;
+        }
+
+        massaFinal = massa;
+        tempo[2] = 0;
+
+        while (tempo[3] >= 60){
+            segundos -= 60;
+            tempo[2]++;
+        }
+        tempo[1] = 0;
+
+        while (minutos >= 60){
+            minutos -= 60;
+            tempo[0]++;
+        }
+
+        System.out.println("Massa inicial: "+massaInicial);
+        System.out.println("Massa final: "+massaFinal);
+        System.out.println("Horas: "+tempo[0]+" Minutos: "+tempo[1]+" Segundos: "+tempo[2]);
+
+
+    }
+}
